3a3a95495a138e09232c67caad0503fda22542b9
Saeed Heydari <s.heydari.7997@gmail.com>
2020-08-06 22:02:28 +0430

    blog post finaly

--- src/component/SideBar/sidebar.js
+++ src/component/SideBar/sidebar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './sidebar.css'
+import './sideBar.css'
 import Card from '../Ui/Card';
 
 /**
@@ -9,9 +9,45 @@ import Card from '../Ui/Card';
 
 const SideBar = (props) => {
   return(
-    <Card width="27%">
-        SideBar
-    </Card>
+    <div className="sidebarContainer">
+      
+      <Card style={{marginBottom:'20px',padding:'20px',boxSizing:'border-box'}}>
+        <div className="cardHeader">
+          <span>About Us</span>
+        </div>
+        <div className="profileimage">
+          <img src="https://www.persiantvseries.com/wp-content/uploads/amir-hossein-arman-photo.jpg"/>
+        </div>
+        <div className="cardBody">
+          <p className="personalBio">My name is saeed heydari im front developer and i live bonab and i study ce .</p>
+        </div>
+      </Card>
+
+      <Card style={{marginBottom:'20px',padding:'20px',boxSizing:'border-box'}}>
+        <div className="cardHeader">
+          <span>Social Network</span>
+        </div>
+      </Card>
+
+      <Card style={{marginBottom:'20px',padding:'20px',boxSizing:'border-box'}}>
+        <div className="cardHeader">
+          <span>Recent Post</span>
+        </div>
+        <div className="recentPosts">
+          <div className="recentPost">
+            <h3>Post Title</h3>
+            <span>July 21 , 2020</span>
+          </div>
+        </div>
+        <div className="recentPosts">
+          <div className="recentPost">
+            <h3>Post Title2</h3>
+            <span>July 23 , 2020</span>
+          </div>
+        </div>
+      </Card>
+
+    </div>
    )
 
  }
--- src/component/blogPost/blogPost.js
+++ src/component/blogPost/blogPost.js
@@ -9,9 +9,29 @@ import Card from './../Ui/Card'
 
 const BlogPost = (props) => {
   return(
-    <Card width='70%'>
-        BlogPost
-    </Card>
+    <div className="blogPostcontainer">
+      <Card>
+        <div className="blogHeader">
+          <span className="blogCategory">Featured</span>
+          <h1 className="postTitle">Beautful is always beautiful</h1>
+          <span className="postBy">posted on oct 21 , 2020 bloging page</span>
+        </div>
+        <div className="postImageContainer">
+          <img src={require("../../blogPostImage/pexels-bruno-bueno-2204204.jpg")} alt="postimg"/>
+        </div>
+        <div className="postContent">
+          <h3>Post Tite</h3>
+          <p>Where does it come from?
+              Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
+
+              The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.
+
+              Where can I get some?
+              There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.
+          </p>
+        </div>
+      </Card>
+    </div>
    )
 
  }
